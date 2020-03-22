4de466650dc45e9e8b00c2195e20f02eb41abb54
Tobias Stuckenberger <tobias.stuckenberger@gmail.com>
2020-03-22 11:26:45 +0100

    Hotfix

--- app/src/main/java/de/flatcorona/FullscreenActivity.java
+++ app/src/main/java/de/flatcorona/FullscreenActivity.java
@@ -1,26 +1,20 @@
 package de.flatcorona;
 
-import android.annotation.SuppressLint;
-
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Activity;
 import android.os.Bundle;
-import android.os.Handler;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.EditText;
 
-import java.io.Console;
+import androidx.appcompat.app.AppCompatActivity;
 
-/**
- * An example full-screen activity that shows and hides the system UI (i.e.
- * status bar and navigation/system bar) with user interaction.
- */
-public class FullscreenActivity extends Activity {
+public class FullscreenActivity extends AppCompatActivity {
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
+        setContentView(R.layout.activity_fullscreen);
+    }
 
     public void full_activity_start(View view){
         EditText username = findViewById(R.id.activity_fullscreen_username);
